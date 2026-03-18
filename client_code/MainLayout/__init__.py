19eb7a715998f3add8aa1510f11cad53392590f1
jakob.seeberger@student.htl-rankweil.at <jakob.seeberger@student.htl-rankweil.at>
2026-03-18 15:06:14 +0000

    Edited MainLayout.Fahrerstatistik, MainLayout.Fahrer and MainLayout

--- client_code/MainLayout/__init__.py
+++ client_code/MainLayout/__init__.py
@@ -1,19 +1,37 @@
 from ._anvil_designer import MainLayoutTemplate
 from anvil import *
 import plotly.graph_objects as go
+from anvil import GoogleMap
 from .DashboardFahrer import DashboardFahrer
 from .DashboardTeams import DashboardTeams
-
 from .Fahrer import Fahrer
 from .Fahrerstatistik import Fahrerstatistik
 from .Teams import Teams
 from .Weltmeister import Weltmeister
 
+
 class MainLayout(MainLayoutTemplate):
   def __init__(self, **properties):
     self.init_components(**properties)
 
-    # Startseite beim Öffnen
+    # Karte initialisieren
+    self.zeige_interlagos()
+
+  def zeige_interlagos(self):
+    center_lat = -23.7014
+    center_lng = -46.6969
+
+    self.map_1.center = GoogleMap.LatLng(center_lat, center_lng)
+    self.map_1.zoom = 15
+
+    # Marker
+    marker = GoogleMap.Marker(
+      position=GoogleMap.LatLng(center_lat, center_lng),
+      title="Interlagos"
+    )
+    self.map_1.add_component(marker)
+
+
 
   def zeige_form(self, form_klasse):
     self.column_panel_1.clear()
@@ -35,10 +53,10 @@ class MainLayout(MainLayoutTemplate):
   def btn_weltmeister_click(self, **event_args):
     self.zeige_form(Weltmeister)
 
-def btn_dashboard_fahrer_click(self, **event_args):
-  self.zeige_form(DashboardFahrer)
+  @handle("btn_dashboard_fahrer", "click")
+  def btn_dashboard_fahrer_click(self, **event_args):
+    self.zeige_form(DashboardFahrer)
 
-def btn_dashboard_teams_click(self, **event_args):
-  self.zeige_form(DashboardTeams)
-
-
+  @handle("btn_dashboard_teams", "click")
+  def btn_dashboard_teams_click(self, **event_args):
+    self.zeige_form(DashboardTeams)
